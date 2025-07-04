8a8882a233cd45c1d563694b7a8e0e9ebce3b3e2
JarekDymek <jarekdymek@gmail.com>
2025-07-04 22:49:53 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,7 @@
 // Plik: sw.js (Service Worker)
 
-const CACHE_NAME = 'strongman-nextgen-cache-v1';
+// --- POPRAWKA: Zmieniamy nazwę cache'u. To zmusi przeglądarkę do pobrania wszystkich plików na nowo. ---
+const CACHE_NAME = 'strongman-nextgen-cache-v2'; 
 const urlsToCache = [
   '/',
   '/index.html',
@@ -17,6 +18,7 @@ const urlsToCache = [
   '/js/persistence.js',
   '/js/state.js',
   '/js/stopwatch.js',
+  '/js/checkpointsDb.js', // Upewnij się, że ten plik też jest na liście
   // Pamiętaj, aby dodać tutaj ścieżki do ikon, gdy je utworzysz
   // '/images/icon-192.png',
   // '/images/icon-512.png'
@@ -24,27 +26,44 @@ const urlsToCache = [
 
 // Instalacja Service Workera i zapisanie zasobów w pamięci podręcznej
 self.addEventListener('install', (event) => {
+  console.log('Service Worker: Instalowanie nowej wersji...');
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then((cache) => {
-        console.log('Opened cache');
+        console.log('Otwarto cache:', CACHE_NAME);
         return cache.addAll(urlsToCache);
       })
   );
+  // Wymuś aktywację nowego Service Workera natychmiast
+  self.skipWaiting();
 });
 
-// Obsługa zapytań sieciowych - serwowanie zasobów z pamięci podręcznej
+// Aktywacja nowego Service Workera i usunięcie starych cache'ów
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker: Aktywacja nowej wersji...');
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Service Worker: Usuwanie starego cache\'u:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  // Przejmij kontrolę nad wszystkimi otwartymi stronami
+  return self.clients.claim();
+});
+
+
+// Obsługa zapytań sieciowych - strategia "sieć najpierw, potem cache"
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        // Jeśli zasób jest w pamięci podręcznej, zwróć go
-        if (response) {
-          return response;
-        }
-        // W przeciwnym razie, pobierz go z sieci
-        return fetch(event.request);
-      }
-    )
+    fetch(event.request).catch(() => {
+      // Jeśli sieć zawiedzie, spróbuj pobrać z cache'u
+      return caches.match(event.request);
+    })
   );
 });
